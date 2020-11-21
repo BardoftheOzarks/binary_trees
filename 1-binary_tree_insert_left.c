e0c5094544bd104406f20fc55ab0002832b18283
GreenCyberNinja <1824@holbertonschool.com>
2020-11-21 19:28:47 +0000

    fixed memory error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new == NULL || parent == NULL)
 		return (NULL);
 	new->n = value;
-	new->parent = parent;
 	new->left = parent->left;
+	if (parent->left != NULL)
+		parent->left->parent = new;
 	parent->left = new;
+	new->parent = parent;
 	return (new);
 }
