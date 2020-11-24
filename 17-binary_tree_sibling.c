2babb5369fcb7ec2cf4e0c13c05a9d0f0f9ceebf
GreenCyberNinja <1824@holbertonschool.com>
2020-11-24 20:34:34 +0000

    betty compliant

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 /**
-*
-*
-*
-*
+* binary_tree_sibling - finds the sibling
+* @node: bode to find its sibling
+* Return: returns sibling node or null
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL)
+		return (NULL);
 	if (node->parent == NULL)
 		return (NULL);
 	if (node->parent->left->n == node->n)
