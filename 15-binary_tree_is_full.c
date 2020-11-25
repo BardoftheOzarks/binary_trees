f82fadbf0bf8ed988686a0b545bb3209de391c8f
GreenCyberNinja <1824@holbertonschool.com>
2020-11-25 02:43:14 +0000

    betty approved

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	int ans;
 
 	if (tree == NULL)
-		return (NULL);
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_is_full(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_is_full(tree->right) : 0;
